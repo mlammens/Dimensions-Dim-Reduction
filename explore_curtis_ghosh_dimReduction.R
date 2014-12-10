37309b1bc694ce9643ad2e713b1b9f1d118c9682
Matthew Aiello-Lammens <matt.lammens@gmail.com>
2014-12-10 16:02:49 -0500

    Changes to allow for modeling of pellies, as well as proteas

--- explore_curtis_ghosh_dimReduction.R
+++ explore_curtis_ghosh_dimReduction.R
@@ -22,6 +22,9 @@ require(R2jags)
 make_new_clim_df <- FALSE
 use_jags_test_pars <- FALSE
 
+#genus <- "Protea"
+genus <- "Pelargonium"
+
 ## -------------------------------------------------------------------- ##
 ## Make a vector of the climate variable names
 clim_vars <- c("CDD","CFD","CSU","GDD",
@@ -35,83 +38,83 @@ clim_vars <- c("CDD","CFD","CSU","GDD",
 ## Get Protea and Pelie datasets
 ## ******************************************************************** ##
 
-## -------------------------------------------------------------------- ##
 ## Read in Protea data.frame
 protea_df <- read.csv( "data/Protea_CFR_DATA.csv", na.strings = "." )
 
-prot_trait <- c( "LMA", "Canopy_area", "LWratio", "FWC", 
-                 "LDMC", "Succulence", "Height", "lamina_thickness" )
-# prot_id <- c( "Year", "Site_location", "Species_name" )
-# 
-# protea_df <- protea_df[ c( prot_id, prot_trait ) ]
+## Read in Pellie data.frame
+pel_df <- read.csv( "data/Pel_CFR_2011_2012.csv", na.strings = "."  )
+
+## Make a vector of the trait names
+traits <- c( "LMA", "Canopy_area", "LWratio", "FWC", 
+             "Height", "lamina_thickness" )
+
 
 ## -------------------------------------------------------------------- ##
-## Read in Pellie data.frame
-pel_df <- read.csv( "data/Pel_CFR_2011_2012.csv" )
+## Combine protea and pellie datasets
+prot_pel_df <- rbind( dplyr::select( protea_df, Site_location, LONGITUDE, LATITUDE,
+                              genus, LMA, Canopy_area, LWratio, FWC, 
+                              Height, lamina_thickness ),
+                      dplyr::select( pel_df, Site_location, LONGITUDE, LATITUDE,
+                              genus, LMA, Canopy_area, LWratio, FWC, 
+                              Height, lamina_thickness ) )
 
 ## -------------------------------------------------------------------- ##
 ## Get climate data
 
 if( make_new_clim_df ){
+  ## Extract lat/lon values for unique Site_locations
+  sites <- filter( prot_pel_df, !duplicated( Site_location ) ) %>% 
+    dplyr::select( Site_location, LONGITUDE, LATITUDE )
+  
   ## Source the Extract_Climate_Vars function
   source( "~/Dropbox/UConn-PostDoc/Projects/Dimensions-GCFR/Dimensions-Data/scripts/Util_ExtractPoints_WilsonSummary.r" )
   ## Get protea location climate values
-  protea_clim <- 
-   Util_ExtractPoints_WilsonSummary( lon = protea_df$LONGITUDE, lat = protea_df$LATITUDE )
+  prot_pel_clim <- 
+   Util_ExtractPoints_WilsonSummary( lon = sites$LONGITUDE, lat = sites$LATITUDE )
   ## Write the climate dataset to file
-  write.csv( protea_clim, file = "protea_clim.csv", row.names = FALSE )
+  write.csv( prot_pel_clim, file = "prot_pel_clim.csv", row.names = FALSE )
 }
 
 ## Read the saved climate values
-protea_clim <- read.csv( "protea_clim.csv" )
+prot_pel_clim <- read.csv( "prot_pel_clim.csv" )
 
 ## Read in the additional climate values, from Mitchell et al. 2014
-prot_pel_clim <- read.csv( "data/Protea_Pellie_Climate.csv" )
+prot_pel_clim_add <- read.csv( "data/Protea_Pellie_Climate.csv" )
 
-## Merge the additional cliamte values with the protea_clim data.frame
-# temp <- 
-#   merge( as.data.frame( protea_clim), 
-#          select( prot_pel_clim, LONGITUDE, LATITUDE, Elevation, Insolation ),
-#          by.x = c( "lon", "lat" ),
-#          by.y = c( "LONGITUDE", "LATITUDE" )
-#   )
-
-#protea_clim <- protea_clim[ clim_vars ]
+## Merge the two climate data.frames
+prot_pel_clim <- 
+  merge( prot_pel_clim, 
+         dplyr::select( prot_pel_clim_add, -(CDD:MAT) ),
+         by.x = c( "lon", "lat"),
+         by.y = c( "LONGITUDE", "LATITUDE" ) )
 
 ## Scale the climate variables
-protea_clim <- scale( protea_clim )
+prot_pel_clim[ clim_vars ] <- scale( prot_pel_clim[ clim_vars ] )
 
-## Add climate to protea_df
-protea_df <- cbind( protea_df, protea_clim )
-
-## Add solar insulation layers
-protea_df <- 
-  merge( protea_df, select( prot_pel_clim, Site_location, Elevation, Insolation ),
+## Merge the trait and climate variables
+prot_pel_df <- 
+  merge( dplyr::select( prot_pel_df, -genus ), 
+         dplyr::select( prot_pel_clim, -(lon:lat) ),
          by = "Site_location" )
-
-## Scale Elevation and Insolation
-protea_df$Elevation <- scale( protea_df$Elevation )
-protea_df$Insolation <- scale( protea_df$Insolation )
-
-# protea_df <- protea_df[ complete.cases( protea_df ), ]
-
-## Scale protea traits
-protea_df[ prot_trait ] <- scale( protea_df[ prot_trait ] )
-
 
 ## ******************************************************************** ##
 ## Run Curtis and Ghosh model using LMA and all climate values
 ## ******************************************************************** ##
 
-## Alternatively extract data as a matrix
-#crime.pred.mat <- 
-X <- as.matrix( protea_df[ clim_vars ] )
+if( genus == "Protea" ){
+  df <- filter( prot_pel_df, genus == "Protea" )
+} else if( genus == "Pelargonium" ){
+  df <- filter( prot_pel_df, genus == "Pelargonium" )
+}
 
-## Separate response variable into it's own vector
-#y <- protea_df$LMA
+## Scale the trait matrices
+df[ traits ] <- scale( df[ traits ] )
+  
+## Extract predictos (i.e., climate) as a matrix
+X <- as.matrix( df[ clim_vars ] )
 
 ## Define model settings
-n.samp <- nrow( protea_df )
+n.samp <- nrow( df )
 
 ## The next parameter, M, is somewhat arbitrarily set. The authors 
 ## write use a "suitably large M", and define it as p + 25, where 
@@ -143,10 +146,9 @@ if( use_jags_test_pars ){
   n.thin <- 5*50
 }
 
+## Make a vector of parameters to track in JAGs
 jags.par <-
   c( "beta", "gamma", "S", "theta", "xi", "pi" )
-
-
 
 ## Use for loop to run different traits overnight
 
@@ -154,16 +156,9 @@ traits <- c( "LMA", "Canopy_area", "FWC", "LWratio" )
 fit <- vector( mode = "list" )
 
 for( i in traits ){
-  y <- protea_df[[ i ]]
   
-  #library( foreach )
-  #library( doSNOW )
-  
-  #cl <- makeCluster( 4, "SOCK" )
-  #registerDoSNOW( cl )
-  
-  #foreach( i = 1:4, .combine = "list", .packages = c("R2jags"), .export = jags.data ) %dopar% {
-  
+  ## Set the response variable 
+  y <- df[[ i ]]  
   
   ## Run jags model
   fit[[ i ]] <-   
@@ -180,168 +175,8 @@ for( i in traits ){
                     working.directory = "." ) )
   
 }  
-#stopCluster( cl )
 
 ## -------------------------------------------------------------------- ##
 
-## Get model outputs
-beta.vals <- as.data.frame( fit.lma$BUGSoutput$sims.list$beta )
-gamma.vals <- as.data.frame( fit.lma$BUGSoutput$sims.list$gamma )
-S.vals <- as.data.frame( fit.lma$BUGSoutput$sims.list$S )
-theta.vals <- as.data.frame( fit.lma$BUGSoutput$sims.list$theta )
-xi.vals <- as.data.frame( fit.lma$BUGSoutput$sims.list$xi )
-deviance.vals <- as.data.frame( fit.lma$BUGSoutput$sims.list$deviance )
-
-## Add names of variables, where appropriate
-names( beta.vals ) <- clim_vars
-names( S.vals ) <- clim_vars
-
-## Make plots to look at S versus Beta
-
-## Melt beta matrix
-beta_m <- melt( beta.vals, value.name = "beta", variable.name = "clim_var" )
-
-## Melt S matrix
-S_melt <- melt( S.vals, value.name = "S" )
-
-## Melt the theta matrix
-theta_melt <- melt( theta.vals, value.name = "theta" )
-
-## Combine the melted matrix values
-jags_vals_mat <- beta_m
-jags_vals_mat$S <- S_melt$S
-jags_vals_mat$theta <- theta_melt$theta
-
-## Plot the histograms of beta values
-ggplot() + geom_histogram( data = jags_vals_mat, aes( x = beta, fill = factor( clim_var ) ) ) +
-  facet_wrap( ~clim_var, scales = "free" ) +
-  geom_vline( xintercept = 0, colour = "red" ) +
-  scale_fill_discrete( name = "Clim Var" )
-ggsave( filename = "figures/prot_lma_beta_hist.pdf", units = "in", width = 10, height = 10 )
-
-## Plot the histograms of beta values, faceted by S values
-ggplot() + geom_density( data = jags_vals_mat, aes( x = beta ) ) +
-  facet_wrap( ~ S )
-  #geom_vline( xintercept = 0, colour = "red" ) +
-  #scale_fill_discrete( name = "Clim Var" )
-ggsave( filename = "figures/prot_lma_beta_hist_by_S.pdf", units = "in", width = 10, height = 10 )
 
 
-## Plot beta (coeff value) versus S (label ID)
-ggplot() + geom_point( data = jags_vals_mat, aes( x = S, y = beta, colour = factor( clim_var ) ) ) + 
-  facet_grid( clim_var~.) + 
-  xlab( "S" ) + 
-  ylab( "beta" ) + 
-  scale_colour_discrete( name = "Clim Var" )
-ggsave( filename = "figures/prot_lma_beta_vs_S.pdf", units = "in", width = 10, height = 10 )
-
-## Plot theta vesus S
-ggplot() + geom_point( data = jags_vals_mat, aes( x = S, y = theta, colour = factor( clim_var ) ) ) + 
-  facet_grid( clim_var~., scales = "free" ) + 
-  xlab( "S" ) + 
-  ylab( "theta" ) + 
-  scale_colour_discrete( name = "Clim Var" )
-ggsave( filename = "figures/prot_lma_theta_vs_S.pdf", units = "in", width = 10, height = 10 )
-
-## Plot theta vesus beta
-ggplot() + geom_point( data = jags_vals_mat, aes( x = beta, y = theta, colour = factor( clim_var ) ) ) + 
-  facet_grid( clim_var~., scales = "free" ) + 
-  xlab( "beta" ) + 
-  ylab( "theta" ) + 
-  scale_colour_discrete( name = "Clim Var" )
-ggsave( filename = "figures/prot_lma_theta_vs_beta.pdf", units = "in", width = 10, height = 10 )
-
-
-## ******************************************************************** ##
-## ******************************************************************** ##
-### !!! DEVELOPMENT COMMENT !!! ###
-## Need to make all the changes that were carried out above, for the rest
-## of the variables.
-## ******************************************************************** ##
-## ******************************************************************** ##
-
-
-
-## -------------------------------------------------------------------- ##
-## Run Curtis and Ghosh model using Canopy and all climate values
-## -------------------------------------------------------------------- ##
-
-## Re-define "y"
-y <- protea_df$Canopy_area
-
-fit.canopy <- 
-  jags( data = jags.data,
-        inits = NULL,
-        parameters = jags.par,
-        model.file = model.file, 
-        n.chains = n.chains,
-        n.burnin = n.burnin,
-        n.iter = n.iter,
-        n.thin = n.thin,
-        DIC = TRUE,
-        working.directory = "." )
-
-## Get model output
-canopy.mu.mean <- fit.canopy$BUGSoutput$mean$mu
-canopy.mu <- fit.canopy$BUGSoutput$sims.list$mu
-canopy.beta.mean <- fit.canopy$BUGSoutput$mean$beta
-rownames( canopy.beta.mean ) <- clim_vars
-canopy.beta <- fit.canopy$BUGSoutput$sims.list$beta
-canopy.beta <- as.data.frame( canopy.beta )
-names( canopy.beta ) <- clim_vars
-
-## Look at boxplots
-ggplot( data = melt( canopy.beta ),
-        aes( x = variable, y = value ) ) +
-  geom_boxplot() +
-  xlab( "Climate predictor variable" ) +
-  ylab( "Regression coefficient" )
-
-## Look at density pltos
-ggplot( data = melt( canopy.beta ),
-        aes( x = value ) ) +
-  geom_density() +
-  facet_wrap( ~variable ) +
-  ylab( "Beta value" )
-
-## -------------------------------------------------------------------- ##
-## Run Curtis and Ghosh model using Canopy and all climate values
-## -------------------------------------------------------------------- ##
-
-## Re-define "y"
-y <- protea_df$LWratio
-
-fit.lwr <- 
-  jags( data = jags.data,
-        inits = NULL,
-        parameters = jags.par,
-        model.file = model.file, 
-        n.chains = n.chains,
-        n.burnin = n.burnin,
-        n.iter = n.iter,
-        n.thin = n.thin,
-        DIC = TRUE,
-        working.directory = "." )
-
-## Get model output
-lwr.mu.mean <- fit.lwr$BUGSoutput$mean$mu
-lwr.mu <- fit.lwr$BUGSoutput$sims.list$mu
-lwr.beta.mean <- fit.lwr$BUGSoutput$mean$beta
-rownames( lwr.beta.mean ) <- clim_vars
-lwr.beta <- fit.lwr$BUGSoutput$sims.list$beta
-lwr.beta <- as.data.frame( lwr.beta )
-names( lwr.beta ) <- clim_vars
-
-## Look at boxplots
-ggplot( data = melt( lwr.beta ),
-        aes( x = variable, y = value ) ) +
-  geom_boxplot() +
-  xlab( "Climate predictor variable" ) +
-  ylab( "Regression coefficient" )
-
-## Look at density pltos
-ggplot( data = melt( lwr.beta ),
-        aes( x = value ) ) +
-  geom_density() +
-  facet_wrap( ~variable ) +
-  ylab( "Beta value" )
